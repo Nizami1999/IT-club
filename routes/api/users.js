852269f68fdc6509eda2485b7ce3bcc527beaad1
Nizami <61557989+Nizami1999@users.noreply.github.com>
2021-11-11 01:09:49 +0100

    Request and Body validation

---
+++ routes/api/users.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+
+// @route   POST api/users
+// @desc    Test route
+// @access  Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please include a password with at least 6 characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send("Success");
+  }
+);
+
+module.exports = router;
