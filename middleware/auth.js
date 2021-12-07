e887258924779b66037ecfb16341126a1ded842c
Nizami <61557989+Nizami1999@users.noreply.github.com>
2021-12-07 10:46:05 +0100

    Back-end completed

--- middleware/auth.js
+++ middleware/auth.js
@@ -21,3 +21,4 @@ module.exports = function (req, res, next) {
     res.status(401).json({ message: "Token is not valid" });
   }
 };
+
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -108,4 +108,160 @@ router.delete("/:post_id", auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/posts/like/:post_id
+// @desc    Like post
+// @access  Private
+router.put("/like/:post_id", auth, async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    if (post.user.toString() === req.user.id) {
+      return res.status(401).json({
+        message: "You can not like your own posts",
+      });
+    }
+
+    // Check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      const index = post.likes
+        .map((like) => like.user.toString())
+        .indexOf(req.user.id);
+
+      post.likes.splice(index, 1);
+
+      await post.save();
+      return res.status(200).json({
+        message: "You return your like from this post",
+        likes: post.likes,
+      });
+    } else {
+      post.likes.unshift({
+        user: req.user.id,
+      });
+
+      await post.save();
+      return res.status(200).json({
+        message: "You liked this post",
+        likes: post.likes,
+      });
+    }
+  } catch (err) {
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+  }
+});
+
+// @route   PUT api/posts/comment/:post_id
+// @desc    Comment post
+// @access  Private
+router.post(
+  "/comment/:post_id",
+  [auth, [check("comment", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      let post = await Post.findById(req.params.post_id);
+      let user = await User.findById(req.user.id).select("-password");
+
+      if (!post) {
+        return res.status(404).json({
+          message: "Post not found",
+        });
+      }
+
+      const { comment } = req.body;
+
+      let newComment = {
+        user: user.id,
+        comment: comment,
+        name: user.name,
+        avatar: user.avatar,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      return res.status(200).json({
+        message: "Comment was successfully added",
+        comments: post.comments,
+      });
+    } catch (err) {
+      if (err.kind === "ObjectId") {
+        return res.status(404).json({
+          message: "Post not found",
+        });
+      }
+    }
+  }
+);
+
+// @route   DELETE api/posts/comment/:post_id/:comment_id
+// @desc    Delete comment
+// @access  Private
+router.delete("/comment/:post_id/:comment_id", auth, async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // Either comment exist or not
+    if (!comment) {
+      return res.status(404).json({
+        message: "Comment does not exist",
+      });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({
+        message: "User not authorized",
+      });
+    }
+
+    const index = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(index, 1);
+
+    await post.save();
+
+    return res.status(200).json({
+      message: "Comment was successfully deleted",
+      comments: post.comments,
+    });
+  } catch (err) {
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+  }
+});
+
 module.exports = router;
